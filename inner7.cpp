c3a4e2e2651d14eb5c74855c8ed1b4da866f9754
vivekdwived <42137907+vivekdwived@users.noreply.github.com>
2018-10-12 23:28:38 +0530

    Update inner7.cpp

--- inner7.cpp
+++ inner7.cpp
@@ -1,38 +1,39 @@
-		//7. Write a C++ program to check whether a number is palindrome or not using recursion.
+	//7. Write a C++ program to check whether a number is palindrome or not using recursion.
 
-#include <iostream>
-using namespace std;
+	#include <iostream>
+	using namespace std;
 
-/*
-palindrome --no.=its reverse
-1.get a no.
-2. find its reverse ----use n= int (n/10)/..till n=0..call function inside function store the remainder r= n%10...as r1 r2 r3 r4..etc
-*/
+	/*
+	palindrome --no.=its reverse
+	1.get a no.
+	2. find its reverse ----use n= int (n/10)/..till n=0..call function inside function store the remainder r= n%10...as r1 r2 r3 r4..etc
+	*/
 
-int reverseSum(int n,int sum=0)
-{	int p=n%10;
+	int reverseSum(int n,int sum=0)
+	{	int p=n%10;
 	
-	sum+=p;
+		sum+=p;
 	
-	if(n==0)
-	return sum;
-	else 
-	sum*=10;
-	return reverseSum(n/10,sum);
-}
+		if(n==0)
+		return sum;
+		else 
+		sum*=10;
+		return reverseSum(n/10,sum);
+	}
 
 
 
-int main()
-{	int n,c;
-	cout<<"enter the no.  & I shall tell u whether it is PALINDROME OR NOT :"<<endl;
-	cin>>n;
+	int main()
+	{	int n,c;
+		cout<<"enter the no.  & I shall tell u whether it is PALINDROME OR NOT :"<<endl;
+		cin>>n;
 
-	int p;
-	c=reverseSum(n);
-	if(n==c)
-	cout<<"given no. is PALINDROME";
-	else
-	cout<<"given no. 
-return 0;
-}
+		int p;
+		c=reverseSum(n);
+		//cout<<c/10;
+		if(n==c/10)
+		cout<<"given no. is PALINDROME";
+		else
+		cout<<"given no. is not PALINDROME";
+	return 0;
+	}
